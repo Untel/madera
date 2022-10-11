751dabcc54e00651670a4a3dc6e076a0650123a0
Adrien Fernandes <adda-sil@student.42.fr>
2022-10-11 16:35:15 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCg85Y2ZuhY-4xjizfsDA0HVY10MKifcCU',
+    apiKey: '',
     authDomain: 'madera-8ef40.firebaseapp.com',
     databaseURL: 'https://madera-8ef40.firebaseio.com',
-    projectId: 'madera-8ef40',
+    projectId: '',
     storageBucket: 'madera-8ef40.appspot.com',
-    messagingSenderId: '386248655226'
+    messagingSenderId: ''
   },
 };
